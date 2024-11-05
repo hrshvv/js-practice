a16be35e9a9476909dd7f6ca1f71b1a86fb0ea83
hrshvv <harshver.727@gmail.com>
2024-11-05 22:38:56 +0530

    first commit

---
+++ harsh.js
@@ -0,0 +1,10 @@
+const outputElement = document.getElementById("output");
+
+for (let a = 1; a <= 100; a++) {
+  if (a % 2 == 0) {
+    outputElement.innerHTML += "hacchuuuu<br>";
+  } else {
+    outputElement.innerHTML += a + "<br>";
+  }
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
