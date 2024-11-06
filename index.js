431e6c9d49f92f18fde5c0f9fbcfdc021e78d585
hrshvv <harshver.727@gmail.com>
2024-11-06 23:44:27 +0530

    if else

---
+++ index.js
@@ -0,0 +1,27 @@
+let a = "dyucwhcjkdwm";
+
+let b = 543;
+let h = true;
+
+let yash = [true, 32842, "rebiyhejkf", b, {}];
+
+let harsh = {
+  is_harsh_a_male: true,
+  bank_balance: 473694,
+  real_name: "harsh verma",
+  things: ["phone", "laptop"],
+};
+
+console.log(yash[3]);
+
+function gf(a, b) {
+  return a + b;
+}
+
+
+console.log(gf(1, 2));
+
+
+
+
+
---
+++ p1.js
@@ -0,0 +1,9 @@
+let FirstName = "Harsh";
+let age = 18;
+let isMarried = false;
+
+if (isMarried == true) {
+  console.log(FirstName + "is not married");
+} else {
+  console.log(FirstName + "is not married");
+}
